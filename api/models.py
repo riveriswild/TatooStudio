de94043a5a59e0e314f8b3951b63e2150c8423ee
River <riveriswild@icloud.com>
2022-06-20 00:41:00 +0300

    Changed Master model to Singleton

--- api/models.py
+++ api/models.py
@@ -1,7 +1,34 @@
 from django.db import models
+from django.core.cache import cache
 
 
-class Master(models.Model):
+class SingletonModel(models.Model):
+
+    class Meta:
+        abstract = True
+
+    def delete(self, *args, **kwargs):
+        pass
+
+    def set_cache(self):
+        cache.set(self.__class__.__name__, self)
+
+    def save(self, *args, **kwargs):
+        self.pk = 1
+        super(SingletonModel, self).save(*args, **kwargs)
+
+        self.set_cache()
+
+    @classmethod
+    def load(cls):
+        if cache.get(cls.__name__) is None:
+            obj, created = cls.objects.get_or_create(pk=1)
+            if not created:
+                obj.set_cache()
+        return cache.get(cls.__name__)
+
+
+class Master(SingletonModel):
     """
     Tattoo artists main info
     """
