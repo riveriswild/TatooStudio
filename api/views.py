9981f43cf771fbd07f77c70e8baabd31128d16f8
River <riveriswild@icloud.com>
2022-07-21 23:56:52 +0300

    comments

--- api/views.py
+++ api/views.py
@@ -8,36 +8,43 @@ from .serializers import MasterSerializer,InfoSerializer, GallerySerializer, \
 
 
 class MasterViewSet(viewsets.ModelViewSet):
+    """ main page """
     serializer_class = MasterSerializer
     queryset = Master.objects.all()
 
 
 class InfoViewSet(viewsets.ModelViewSet):
+    """ info """
     serializer_class = InfoSerializer
     queryset = Info.objects.all()
 
 
 class GalleryViewSet(viewsets.ModelViewSet):
+    """ gallery """
     serializer_class = GallerySerializer
     queryset = Gallery.objects.all()
 
 
 class WorkViewSet(viewsets.ModelViewSet):
+    """ work examples """
     serializer_class = WorkSerializer
     queryset = Work.objects.all()
 
 
 class ReviewViewSet(viewsets.ModelViewSet):
+    """ clients' reviews"""
     serializer_class = ReviewSerializer
     queryset = Review.objects.all()
 
 
 class ApplicationViewSet(viewsets.ModelViewSet):
+    """ tattoo application """
     serializer_class = ApplicationSerializer
     queryset = Application.objects.all()
 
 
 class PriceViewSet(viewsets.ModelViewSet):
+    """ prices page """
     serializer_class = PriceSerializer
     queryset = Price.objects.all()
 
