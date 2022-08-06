1fe9f0b7c7afd839e4780495f75a01fd23ec441d
River <riveriswild@icloud.com>
2022-08-07 01:38:45 +0300

    Register models

--- api/admin.py
+++ api/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Master, Info, Gallery, Application, Price, Work, Review
+from .models import Master, Info, Gallery, Application, Price, Work, Review, FAQ
 
 admin.site.register(Master)
 admin.site.register(Info)
@@ -9,4 +9,5 @@ admin.site.register(Application)
 admin.site.register(Price)
 admin.site.register(Work)
 admin.site.register(Review)
+admin.site.register(FAQ)
 # Register your models here.
