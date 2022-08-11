0bb5c6057314bfd0aab8a3b43fb9542ee0afbde3
River <riveriswild@icloud.com>
2022-08-12 00:04:10 +0300

    fix admin

--- api/admin.py
+++ api/admin.py
@@ -1,6 +1,19 @@
 from django.contrib import admin
+from django import forms
+
+from ckeditor.widgets import CKEditorWidget
 
 from .models import Master, Info, Gallery, Application, Price, Work, Review, FAQ
+
+# class MasterAdminForm(forms.ModelForm):
+#     description = forms.CharField(widget=CKEditorWidget())
+#     class Meta:
+#         model = Master
+#         fields = '__all__'
+#
+# class MasterAdmin(admin.ModelAdmin):
+#     form = MasterAdminForm
+
 
 admin.site.register(Master)
 admin.site.register(Info)
